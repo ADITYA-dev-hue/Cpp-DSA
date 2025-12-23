1d5857403d20751a8c6295531ecc81dbe3161740
Aditya Prakash <adityaprakashlpu@gmail.com>
2025-12-23 11:17:04 +0530

    Updated tamplate

--- C++ LPU/EXCEPTION HANDLING/TRY/tamplate.cpp
+++ C++ LPU/EXCEPTION HANDLING/TRY/tamplate.cpp
@@ -11,6 +11,15 @@ class Base{
     }
 
 };
+
+template<typename T1>
+class child:public Base<T1>{
+    public:
+    int  y;
+    child(T1 val):Base<T1>(val){
+        y=val;
+    }
+};
 int main(){
     vector<int>v={1,2,3,4,5};
 
@@ -25,12 +34,18 @@ int main(){
     Base<string>obj3("Hello Template");
     cout<<obj3.x<<endl;
 
-    
+
     cout<<"-------------------"<<endl;
     Base<vector<int>>obj4(v);
     cout<<"Vector elements: ";
     for(int i=0;i<obj4.x.size();i++){
         cout<<obj4.x[i]<<" ";
     }
+
+    cout<<endl;
+    cout<<"-------------------"<<endl;
+
+    child<int>obj5(10);
+    cout<<"Value from Base class using child class object: "<<obj5.x<<endl;
     return 0;
 }
