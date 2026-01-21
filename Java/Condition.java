57a2c1c6ee66730219a19d22be4adb1583719be9
Aditya Prakash <adityaprakashlpu@gmail.com>
2026-01-21 15:40:10 +0530

    JAVA BEGINS

---
+++ Java/Condition.java
@@ -0,0 +1,20 @@
+class Condition{
+	public static void main(String args[]){
+		int i=12;
+		int j=30;
+		if(i<=18){
+			System.out.println("i not allow");
+		}else{
+			System.out.println("i allowed");
+		}
+		
+		if(j<=18){
+			System.out.println("j not allow");
+		}else{
+			System.out.println("j allowed");
+		}
+
+		
+	
+	}
+}
---
+++ Java/Lecture1.java
@@ -0,0 +1,7 @@
+class Lecture1{
+		public static void main(String args[]){
+        System.out.println("Java Programing");
+		System.out.print("Hello World");
+	}
+
+}
