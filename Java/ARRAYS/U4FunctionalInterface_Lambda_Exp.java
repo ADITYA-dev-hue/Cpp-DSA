96162745ea50c1506749dd3176106ed3936f66ef
Aditya Prakash <adityaprakashlpu@gmail.com>
2026-03-25 14:28:41 +0530

    Lambda Update

--- Java/ARRAYS/U4FunctionalInterface_Lambda_Exp.java
+++ Java/ARRAYS/U4FunctionalInterface_Lambda_Exp.java
@@ -1,12 +1,17 @@
 @FunctionalInterface
 interface Calculator{
-    int add(int a,int b);
+    int operation(int a,int b);
 }
 
 public class U4FunctionalInterface_Lambda_Exp {
     public static void main(String Args[]){
 
-        Calculator c1=(a,b)->a+b;       //-> for lambda.
-        System.out.println(c1.add(5,6));
+        Calculator c1=(a,b)->a+b;       //"->" for lambda.
+        Calculator c2=(a,b)->a*b; //With Lambda_Expression we can perform multiple Expression's with one FuctionalInterface Method
+        Calculator c3=(a,b)->a-b;
+
+        System.out.println("Sum: "+c1.operation(5,6));
+        System.out.println("Product: "+c2.operation(5,6));
+        System.out.println("Difference: "+c3.operation(5,6));
     }
 }
