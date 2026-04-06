63d97784efdcccee3d985c6a2bc4745bee943767
Aditya Prakash <adityaprakashlpu@gmail.com>
2026-04-06 17:57:24 +0530

    Phone Number Exception

---
+++ Java/ARRAYS/ExamplePhNoValid.java
@@ -0,0 +1,39 @@
+class PhoneException extends Exception{
+    PhoneException(String msg){
+        super(msg);
+    }
+}
+class Phone{
+    String ph;
+    Phone(String ph){
+        this.ph=ph;
+    }
+    void PhValidity() throws PhoneException{
+        char[] ch=ph.toCharArray();
+        for(int i=0;i<ch.length;i++){
+         if(!(Character.isDigit(ch[i]))){
+             throw new IllegalArgumentException("Invalid: It is Not of valid");
+         }
+        }
+        if(ph.length()!=10){
+            throw new PhoneException("Invalid: Phone no. should be of 10-digits.");
+       }else{
+        System.out.println("Valid No.");
+       }
+       
+    }
+}
+public class ExamplePhNoValid {
+    public static void main(String args[]){
+        String ph="76194486941";
+        try{
+            Phone p=new Phone(ph);
+            p.PhValidity();
+        }catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }catch(PhoneException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
