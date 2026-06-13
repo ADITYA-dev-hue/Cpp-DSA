4087f06d3c47e67a20c16f991a25d60e9ffdef38
Aditya Prakash <adityaprakashlpu@gmail.com>
2026-06-13 19:44:06 +0530

    Factorial using recursion

---
+++ DSA Striver/RECURSION/factorial_n.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int fact_r(int n){
+    if(n==0){
+        return 1;
+    }
+    if(n==1){
+        return 1;
+    }
+    return fact_r(n-1)*n;
+}
+int main(){
+    int n;
+    cin>>n;
+
+    cout<<n<<"! = "<<fact_r(n);
+}
